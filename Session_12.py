a0243edfe4809614ba163d1428e1443d25d0f7e0
Syed Rizwan <syedbbictech@gmail.com>
2026-09-16 12:51:34 +0530

    Add files via upload

---
+++ Session_12.py
@@ -0,0 +1,57 @@
+#Bitwise Operators
+#a=10  
+#b=6
+#print(a&b)
+#print(10|6)
+
+"""a=10
+b=6
+print(a^b)"""
+
+"""a=10
+print(a<<1)  #20
+print(a<<3)  #80
+
+
+print(10>>1)
+print(10>>2)"""
+
+#Membership Operators
+"""my_List = [1, 2, 3, 4, 5]
+a=3
+b=7
+print(a in my_List)  #True
+print(b in my_List)  #False
+
+print(a not in my_List) #False
+print(b not in my_List)""" #True
+
+
+"""x=10
+y=10
+print(x==y)"""
+
+
+"""x=10
+y=10
+print(id(x))
+print(id(y))
+
+print(x is y)
+print(x is not y)"""
+
+"""x=10
+y=20
+z=5
+if x>=y:  #C-1
+    print("x is greaterthan y")
+elif y>=x:
+    print("y is lessthan x")
+elif z<=x:
+    print("z is lessthan x")
+else:
+    print("All conditions are false")"""
+    
+    
+
+
